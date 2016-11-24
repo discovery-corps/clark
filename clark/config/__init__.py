4dafc0aeb8a41e18bc7b366ca6d239f53fb6a133
Tom Strickx <tom@strickx.com>
2016-11-24 01:47:53 +0100

    Initial configuration management
    Add initial classes for configuration management. Allow modules to override and expand runtime configuration. Allow dynamic loading/reloading of modules.

---
+++ clark/config/__init__.py
@@ -0,0 +1,22 @@
+import json
+from argparse import Namespace
+
+
+class BaseConfig:
+
+    def __init__(self, path):
+        self.path = path
+        self.config = False
+        self.load()
+
+    def load(self):
+        with open(self.path) as data_in:
+            self.config = json.load(data_in, object_hook=lambda d: Namespace(**d))
+
+
+class ModuleConfig(BaseConfig):
+
+    def __init__(self, path, root_config):
+        super().__init__(path)
+        self.config = root_config
+        self.load()
